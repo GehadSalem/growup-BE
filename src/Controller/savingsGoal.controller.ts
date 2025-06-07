d462d9c69007704f06534ed186d7bf4f1c7c487e
Gehad Salem Abd El-shafy <35219807+GehadSalem@users.noreply.github.com>
2025-06-08 00:08:54 +0300

    commit

--- src/Controller/savingsGoal.controller.ts
+++ src/Controller/savingsGoal.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import '../types/express';
-import { SavingsGoalService } from '../Services/SavingsGoal.Service';
+import { SavingsGoalService } from '../Services/savingsGoals.service';
 import { UserService } from '../Services/users.service';
 
 class SavingsGoalController {
--- src/Services/report.service.ts
+++ src/Services/report.service.ts
@@ -1,6 +1,6 @@
 import { ExpenseService } from './expense.service';
 import { MajorGoalService } from './majorGoal.service';
-import { SavingsGoalService } from '../Services/SavingsGoal.Service';
+import { SavingsGoalService } from './savingsGoals.service';
 
 export class ReportService {
     private expenseService = new ExpenseService();
